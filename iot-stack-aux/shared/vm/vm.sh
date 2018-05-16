b411a75b1224753e15e2e46dd70cc570ce3fe73f
kuiyu <kuiyu@microsoft.com>
2018-05-16 15:55:34 -0700

    vm setup

---
+++ iot-stack-aux/shared/vm/vm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+help()
+{
+    echo "This script does the Unbuntu VM setup"
+}
+
+if [ "${UID}" -ne 0 ];
+then
+    log "Script executed without root permissions"
+    echo "You must be root to run this program." >&2
+    exit 3
+fi
+
+#Format the data disk
+bash vm-disk-utils-0.1.sh -s
