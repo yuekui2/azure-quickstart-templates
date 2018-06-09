e1595d7cdbd068410dc45520b82d2f0aecc144c2
kuiyu <kuiyu@microsoft.com>
2018-06-09 10:48:42 -0700

    install with admin credential instead of system

--- iot-stack-aux/allInOne/init.ps1
+++ iot-stack-aux/allInOne/init.ps1
@@ -1,14 +1,18 @@
+param([string]$username = "u", [securestring]$securePassword = "p")
+
+$credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
+Start-Process Notepad.exe
+
 invoke-webrequest -UseBasicparsing -Outfile cosmosdb-emulator.msi https://aka.ms/cosmosdb-emulator
-Start-Process .\cosmosdb-emulator.msi -ArgumentList "/quiet" -Wait
-& 'C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe'
+Start-Process .\cosmosdb-emulator.msi -ArgumentList "/quiet" -Wait -Credential $credential
 
 invoke-webrequest -UseBasicparsing -Outfile docker_ce_win.exe https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe
-& .\docker_ce_win.exe install --quiet
+Start-Process .\docker_ce_win.exe -ArgumentList "install --quiet" -Wait -Credential $credential
 
 # Use "-NoRestart" to avoid restart pop up.
 Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Hyper-V, Containers -All
 
-#$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-#Register-ScheduledJob -Trigger $trigger -FilePath .\runEmulatorAndContainers.ps1 -Name EmulatorAndContainers
+$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
+Register-ScheduledJob -Trigger $trigger -FilePath .\runEmulatorAndContainers.ps1 -Name EmulatorAndContainers
 
 Restart-Computer -Force
