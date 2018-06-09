239c11fbb4c88c612d87a048e76e6b12be8f6978
kuiyu <kuiyu@microsoft.com>
2018-06-08 17:37:21 -0700

    force to restart and add scheduled tasks

---
+++ iot-stack-aux/allInOne/runEmulatorAndContainers.ps1
@@ -0,0 +1,13 @@
+& 'C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe'
+
+DO
+{
+    sleep 5
+    docker info
+} While ($LastExitCode -ne 0)
+
+docker stop iot-stack-redis
+docker rm iot-stack-redis
+docker rmi redis
+docker run --name iot-stack-redis -p 6379:6379 -d redis
+
