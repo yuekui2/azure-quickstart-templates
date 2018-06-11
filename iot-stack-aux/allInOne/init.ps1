e740b567c1deb7d039a5f61a1a565ea29428d27f
kuiyu <kuiyu@microsoft.com>
2018-06-11 16:53:10 -0700

    Revert "add entry.ps1 to overcome credential"
    This reverts commit b2474a6afa04b41db3c2d330fab5ee9806dd872e.

--- iot-stack-aux/allInOne/init.ps1
+++ iot-stack-aux/allInOne/init.ps1
@@ -1,8 +1,13 @@
+param([string]$username = "u", [string]$pwd = "p")
+
+$securePassword = $pwd | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
+
 invoke-webrequest -UseBasicparsing -Outfile cosmosdb-emulator.msi https://aka.ms/cosmosdb-emulator
-Start-Process .\cosmosdb-emulator.msi -ArgumentList "/quiet" -Wait
+Start-Process .\cosmosdb-emulator.msi -ArgumentList "/quiet" -Wait -Credential $credential
 
 invoke-webrequest -UseBasicparsing -Outfile docker_ce_win.exe https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe
-Start-Process .\docker_ce_win.exe -ArgumentList "install --quiet" -Wait
+Start-Process .\docker_ce_win.exe -ArgumentList "install --quiet" -Wait -Credential $credential
 
 # Use "-NoRestart" to avoid restart pop up.
 Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Hyper-V, Containers -All
