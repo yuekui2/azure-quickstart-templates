a5d67c8e21b5d1638d532c9bb2a68e65724dde01
kuiyu <kuiyu@microsoft.com>
2018-06-11 17:04:22 -0700

    experiment to add user to docker-users

--- iot-stack-aux/allInOne/init.ps1
+++ iot-stack-aux/allInOne/init.ps1
@@ -1,18 +1,21 @@
 param([string]$username = "u", [string]$pwd = "p")
 
-$securePassword = $pwd | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
-
 invoke-webrequest -UseBasicparsing -Outfile cosmosdb-emulator.msi https://aka.ms/cosmosdb-emulator
-Start-Process .\cosmosdb-emulator.msi -ArgumentList "/quiet" -Wait -Credential $credential
+Start-Process .\cosmosdb-emulator.msi -ArgumentList "/quiet" -Wait
 
 invoke-webrequest -UseBasicparsing -Outfile docker_ce_win.exe https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe
-Start-Process .\docker_ce_win.exe -ArgumentList "install --quiet" -Wait -Credential $credential
+Start-Process .\docker_ce_win.exe -ArgumentList "install --quiet" -Wait
 
 # Use "-NoRestart" to avoid restart pop up.
 Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Hyper-V, Containers -All
 
-$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-Register-ScheduledJob -Trigger $trigger -FilePath .\runEmulatorAndContainers.ps1 -Name EmulatorAndContainers
+Import-Module ServerManager
+Add-WindowsFeature RSAT-AD-PowerShell
+import-module activedirectory
+Add-LocalGroupMember -Group docker-users -Member $username
+
+
+#$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
+#Register-ScheduledJob -Trigger $trigger -FilePath .\runEmulatorAndContainers.ps1 -Name EmulatorAndContainers
 
 Restart-Computer -Force
