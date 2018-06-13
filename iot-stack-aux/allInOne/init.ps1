08011b4ce259233fe9f91645f308bec8f0eee457
kuiyu <kuiyu@microsoft.com>
2018-06-13 13:53:43 -0700

    add job schedule retry

--- iot-stack-aux/allInOne/init.ps1
+++ iot-stack-aux/allInOne/init.ps1
@@ -31,11 +31,43 @@ $action = {
     docker run -d -p 8080:8080 trinitronx/python-simplehttpserver
 }
 
-$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-DO
-{
-    sleep 5
-    Register-ScheduledJob -Trigger $trigger -ScriptBlock $action -Name EmulatorAndContainers -Credential $credential
-} While ($LastExitCode -ne 0)
+function Retry-Command {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position=0, Mandatory=$true)]
+        [scriptblock]$ScriptBlock,
+
+        [Parameter(Position=1, Mandatory=$false)]
+        [int]$Maximum = 5
+    )
+
+    Begin {
+        $cnt = 0
+    }
+
+    Process {
+        do {
+            $cnt++
+            $cnt >> 'jobScheduleLog.txt'
+            try {
+                sleep 5
+                $ScriptBlock.Invoke()
+
+                return
+            } catch {
+                Write-Error $_.Exception.InnerException.Message -ErrorAction Continue
+            }
+        } while ($cnt -lt $Maximum)
+
+        # Throw an error after $Maximum unsuccessful invocations. Doesn't need
+        # a condition, since the function returns upon successful invocation.
+        throw 'Execution failed.'
+    }
+}
+
+Retry-Command {
+    $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
+    Register-ScheduledJob -Trigger $trigger -ScriptBlock $action -Name EmulatorAndContainers -Credential $credential -ErrorAction Stop
+} -Maximum 20
 
 Restart-Computer -Force
