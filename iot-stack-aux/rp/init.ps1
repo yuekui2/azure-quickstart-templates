9b24a8f3885012374b05ed1ef7ef6e494050f132
kuiyu <kuiyu@microsoft.com>
2018-07-06 18:39:58 -0700

    open sql server port on firewall

--- iot-stack-aux/rp/init.ps1
+++ iot-stack-aux/rp/init.ps1
@@ -20,20 +20,23 @@ Foreach ($path in $paths)
     }
 }
 
+# Open firewall for sql server port
+New-NetFirewallRule -DisplayName "Allow http 1433" -Direction Inbound -Protocol TCP -LocalPort 1433
+
 # Change default database file location
-& sqlcmd -i "setup.sql" -o "output.txt"
+& sqlcmd -i "setup.sql" -o "output1.txt"
 
 # Restart to make it effective
 Restart-Service -Force MSSQLSERVER
 
 # Create a test DB
-& sqlcmd -i "createTestDB.sql" -o "output.txt"
+& sqlcmd -i "createTestDB.sql" -o "output2.txt"
 # Enable and create SQL server login.
-& sqlcmd -v UserName=$username -v Password=$pwd -i "CreateSqlLogins.sql" -o "output.txt"
+& sqlcmd -v UserName=$username -v Password=$pwd -i "CreateSqlLogins.sql" -o "output3.txt"
 # Create tables for IOT hub
-& sqlcmd -i "CreateIotHubProvisioningSchema.sql" -o "output.txt"
-& sqlcmd -i "CreateIotHubProvisioningLogic.sql" -o "output.txt"
-& sqlcmd -i "CreateIotHubProvisioningVersionData.sql" -o "output.txt"
-& sqlcmd -i "CreateIotHubProvisioningData.sql" -o "output.txt"
-& sqlcmd -i "IotDpsProvisioningSchema.sql" -o "output.txt"
-& sqlcmd -i "IotDpsProvisioningLogic.sql" -o "output.txt"
+& sqlcmd -i "CreateIotHubProvisioningSchema.sql" -o "output4.txt"
+& sqlcmd -i "CreateIotHubProvisioningLogic.sql" -o "output5.txt"
+& sqlcmd -i "CreateIotHubProvisioningVersionData.sql" -o "output6.txt"
+& sqlcmd -i "CreateIotHubProvisioningData.sql" -o "output7.txt"
+& sqlcmd -i "IotDpsProvisioningSchema.sql" -o "output8.txt"
+& sqlcmd -i "IotDpsProvisioningLogic.sql" -o "output9.txt"
