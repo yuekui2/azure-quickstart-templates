60b65edc8a0dda348e8bc0a44bdb7efe656a0631
kuiyu <kuiyu@microsoft.com>
2018-07-06 18:53:52 -0700

    add sql server auth to testDB

--- iot-stack-aux/rp/CreateSqlLogins.sql
+++ iot-stack-aux/rp/CreateSqlLogins.sql
@@ -13,3 +13,13 @@ BEGIN
     EXEC sp_addrolemember N'db_owner', N'$(UserName)'
 END;
 GO
+
+Use testDB;
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'$(UserName)')
+BEGIN
+    CREATE USER [$(UserName)] FOR LOGIN [$(UserName)]
+    EXEC sp_addrolemember N'db_owner', N'$(UserName)'
+END;
+GO
