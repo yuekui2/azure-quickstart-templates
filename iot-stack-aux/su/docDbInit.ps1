408c9a8fca910c3817e36d51948e82bde06fe3b7
kuiyu <kuiyu@microsoft.com>
2018-07-11 14:57:31 -0700

    make docDB part of single script

--- iot-stack-aux/su/docDbInit.ps1
+++ iot-stack-aux/su/docDbInit.ps1
@@ -1,6 +1,8 @@
 invoke-webrequest -UseBasicparsing -Outfile cosmosdb-emulator.msi https://aka.ms/cosmosdb-emulator
 Start-Process .\cosmosdb-emulator.msi -ArgumentList "/quiet" -Wait
-Start-Process 'C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe' -ArgumentList "/NoUI"
+Start-Process 'C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe' -ArgumentList "/NoUI /AllowNetworkAccess /Key=lf2YxcQQS1etfXeEsxFavN7k4isJOjOC+wnJuUbZnvBUzMe7GsHg5SQXTI8nQyTXkM3i2eJOCE3nFvP7N//2CQ== /Consistency=Strong /PartitionCount=100"
+
+
 
 # Open firewall
 New-NetFirewallRule -DisplayName "Allow http 10251" -Direction Inbound -Protocol TCP -LocalPort 10251
