b4752662166bc8727e556e94bb9e96018491a2a3
kuiyu <kuiyu@microsoft.com>
2018-07-11 11:55:22 -0700

    fix disk for doc DB

--- iot-stack-aux/su/docDbInit.ps1
+++ iot-stack-aux/su/docDbInit.ps1
@@ -1,3 +1,10 @@
 invoke-webrequest -UseBasicparsing -Outfile cosmosdb-emulator.msi https://aka.ms/cosmosdb-emulator
 Start-Process .\cosmosdb-emulator.msi -ArgumentList "/quiet" -Wait
 Start-Process 'C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe' -ArgumentList "/NoUI"
+
+# Open firewall
+New-NetFirewallRule -DisplayName "Allow http 10251" -Direction Inbound -Protocol TCP -LocalPort 10251
+New-NetFirewallRule -DisplayName "Allow http 10252" -Direction Inbound -Protocol TCP -LocalPort 10252
+New-NetFirewallRule -DisplayName "Allow http 10253" -Direction Inbound -Protocol TCP -LocalPort 10253
+New-NetFirewallRule -DisplayName "Allow http 10254" -Direction Inbound -Protocol TCP -LocalPort 10254
+New-NetFirewallRule -DisplayName "Allow http 8081" -Direction Inbound -Protocol TCP -LocalPort 8081
