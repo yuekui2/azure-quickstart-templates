a6d1876cc6a691db4dab7ebe421e6c2095eed866
kuiyu <kuiyu@microsoft.com>
2018-07-12 14:55:39 -0700

    experiment of file share access

--- iot-stack-aux/su/jumpboxWin.ps1
+++ iot-stack-aux/su/jumpboxWin.ps1
@@ -2,15 +2,7 @@ param([string]$username = "u", [string]$pwd = "p", [string]$computerName = "c")
 
 $certsFolder = "certs"
 $outputpath = "\\" + $computerName + "\" + $certsFolder + "\DocDbSslCert.pfx"
-
-$cnt = 0
-$ret = $false
-DO {
-    Write-Host $cnt
-    sleep 5
-    $cnt++
-    $ret = [System.IO.File]::Exists($outputpath)
-} While (($cnt -lt 5) -and (!$ret))
-
+copy $outputpath
 $pwd = ConvertTo-SecureString -String $pwd -Force -AsPlainText
-Import-PfxCertificate -CertStoreLocation cert:\LocalMachine\Root -FilePath $outputpath -Password $pwd
+#Import-PfxCertificate -CertStoreLocation cert:\LocalMachine\Root -FilePath $outputpath -Password $pwd
+Import-PfxCertificate -CertStoreLocation cert:\LocalMachine\Root -FilePath "DocDbSslCert.pfx" -Password $pwd
