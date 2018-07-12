a3fadbce6bf2b1e95d4eb34c9bfce3c0d3fc385e
kuiyu <kuiyu@microsoft.com>
2018-07-12 16:32:21 -0700

    give copy credential

--- iot-stack-aux/su/jumpboxWin.ps1
+++ iot-stack-aux/su/jumpboxWin.ps1
@@ -1,8 +1,11 @@
 param([string]$username = "u", [string]$pwd = "p", [string]$computerName = "c")
 
+$securePassword = $pwd | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
+
 $certsFolder = "certs"
 $outputpath = "\\" + $computerName + "\" + $certsFolder + "\DocDbSslCert.pfx"
-copy $outputpath
+copy $outputpath -Credential $credential
 $pwd = ConvertTo-SecureString -String $pwd -Force -AsPlainText
 #Import-PfxCertificate -CertStoreLocation cert:\LocalMachine\Root -FilePath $outputpath -Password $pwd
 Import-PfxCertificate -CertStoreLocation cert:\LocalMachine\Root -FilePath "DocDbSslCert.pfx" -Password $pwd
