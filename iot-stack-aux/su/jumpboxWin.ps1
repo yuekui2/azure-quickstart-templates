8d023b79b30946cb9afaad5d5d52315390f937f3
kuiyu <kuiyu@microsoft.com>
2018-07-12 14:14:35 -0700

    add retry for file share

--- iot-stack-aux/su/jumpboxWin.ps1
+++ iot-stack-aux/su/jumpboxWin.ps1
@@ -2,5 +2,15 @@ param([string]$username = "u", [string]$pwd = "p", [string]$computerName = "c")
 
 $certsFolder = "certs"
 $outputpath = "\\" + $computerName + "\" + $certsFolder + "\DocDbSslCert.pfx"
+
+$cnt = 0
+$ret = $false
+DO {
+    Write-Host $cnt
+    sleep 5
+    $cnt++
+    $ret = [System.IO.File]::Exists($outputpath)
+} While (($cnt -lt 5) -and (!$ret))
+
 $pwd = ConvertTo-SecureString -String $pwd -Force -AsPlainText
 Import-PfxCertificate -CertStoreLocation cert:\LocalMachine\Root -FilePath $outputpath -Password $pwd
