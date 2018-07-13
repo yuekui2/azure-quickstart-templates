ddcbfb654b266bbd770c49c515b8dea297a47018
kuiyu <kuiyu@microsoft.com>
2018-07-13 11:10:52 -0700

    deploy script for rp

--- iot-stack-aux/deploy/rp.ps1
+++ iot-stack-aux/deploy/rp.ps1
@@ -1,5 +1,45 @@
 Connect-AzureRmAccount
 
+$index = 6
+$rgName = "iotrprg" + $index
+$deploymentName = "iotrpdeploy" +  $index
+
+New-AzureRmResourceGroup -Name $rgName -Location "eastus"
+
+$namespaceName = "iotrpns"
+$eventhubSku = "Standard"
+$skuCapacity = 1
+$eventHubName = "iotrp"
+$consumerGroupName = "iotrpcg"
+
+$dnsName = "iotstacksqlexdns2" + $index
+$storageAccountName = "iotstacksqlexsa"  + $index
+$adminUsername = "azureuser"
+$pwd = "IotStackAux@1" | ConvertTo-SecureString -AsPlainText -Force
+$vmName = "iotStackSqlExVm"
+$templateBaseUrl = "https://raw.githubusercontent.com/yuekui2/azure-quickstart-templates/master/"
+
+New-AzureRmResourceGroupDeployment `
+    -Name $deploymentName `
+    -ResourceGroupName $rgName `
+    -TemplateFile C:\Users\kuiyu\kafkaAzure\azure-quickstart-templates\iot-stack-aux\rp\rp.json `
+    -Verbose `
+    -DeploymentDebugLogLevel All `
+    -namespaceName $namespaceName `
+    -eventhubSku $eventhubSku `
+    -skuCapacity $skuCapacity `
+    -eventHubName $eventHubName `
+    -consumerGroupName $consumerGroupName `
+    -sqlExNamespace iotStackSqlEx `
+    -sqlExDnsName $dnsName `
+    -sqlExStorageAccountName $storageAccountName `
+    -sqlExVmName $vmName `
+    -sqlExAdminUsername $adminUsername `
+    -sqlExAdminPassword $pwd `
+    -templateBaseUrl $templateBaseUrl
+
+# Below are for individual ATM template testing out
+
 $index = 4
 $rgName = "iotrprg" + $index
 $deploymentName = "iotrpdeploy" +  $index
@@ -44,40 +84,3 @@ New-AzureRmResourceGroupDeployment `
     -templateBaseUrl $templateBaseUrl
 
 
-    $index = 6
-    $rgName = "iotrprg" + $index
-    $deploymentName = "iotrpdeploy" +  $index
-
-    New-AzureRmResourceGroup -Name $rgName -Location "eastus"
-
-    $namespaceName = "iotrpns"
-    $eventhubSku = "Standard"
-    $skuCapacity = 1
-    $eventHubName = "iotrp"
-    $consumerGroupName = "iotrpcg"
-
-    $dnsName = "iotstacksqlexdns2" + $index
-    $storageAccountName = "iotstacksqlexsa"  + $index
-    $adminUsername = "azureuser"
-    $pwd = "IotStackAux@1" | ConvertTo-SecureString -AsPlainText -Force
-    $vmName = "iotStackSqlExVm"
-    $templateBaseUrl = "https://raw.githubusercontent.com/yuekui2/azure-quickstart-templates/master/"
-
-    New-AzureRmResourceGroupDeployment `
-        -Name $deploymentName `
-        -ResourceGroupName $rgName `
-        -TemplateFile C:\Users\kuiyu\kafkaAzure\azure-quickstart-templates\iot-stack-aux\rp\rp.json `
-        -Verbose `
-        -DeploymentDebugLogLevel All `
-        -namespaceName $namespaceName `
-        -eventhubSku $eventhubSku `
-        -skuCapacity $skuCapacity `
-        -eventHubName $eventHubName `
-        -consumerGroupName $consumerGroupName `
-        -sqlExNamespace iotStackSqlEx `
-        -sqlExDnsName $dnsName `
-        -sqlExStorageAccountName $storageAccountName `
-        -sqlExVmName $vmName `
-        -sqlExAdminUsername $adminUsername `
-        -sqlExAdminPassword $pwd `
-        -templateBaseUrl $templateBaseUrl
