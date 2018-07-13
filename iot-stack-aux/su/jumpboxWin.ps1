5cf23c19209e5850398a9db6f17f9aa4dc789e0c
kuiyu <kuiyu@microsoft.com>
2018-07-12 17:22:38 -0700

    experiment of file share access

--- iot-stack-aux/su/jumpboxWin.ps1
+++ iot-stack-aux/su/jumpboxWin.ps1
@@ -1,11 +1,12 @@
 param([string]$username = "u", [string]$pwd = "p", [string]$computerName = "c")
 
-$securePassword = $pwd | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
-
 $certsFolder = "certs"
-$outputpath = "\\" + $computerName + "\" + $certsFolder + "\DocDbSslCert.pfx"
-copy $outputpath -Credential $credential
+$remotepath = "\\" + $computerName + "\" + $certsFolder
+net use $remotepath $pwd /USER:$username
+
+$remotefile =  $remotepath + "\DocDbSslCert.pfx"
+copy $remotefile
+
 $pwd = ConvertTo-SecureString -String $pwd -Force -AsPlainText
 #Import-PfxCertificate -CertStoreLocation cert:\LocalMachine\Root -FilePath $outputpath -Password $pwd
 Import-PfxCertificate -CertStoreLocation cert:\LocalMachine\Root -FilePath "DocDbSslCert.pfx" -Password $pwd
